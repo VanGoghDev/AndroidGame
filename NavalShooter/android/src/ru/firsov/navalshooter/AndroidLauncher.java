534469a26a31f869af4d6858bbadb76326229c12
VanGoghDev <kifirigor@gmail.com>
2018-09-14 23:56:39 +0300

    Project created and back ground image added (need to change image)

---
+++ NavalShooter/android/src/ru/firsov/navalshooter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.firsov.navalshooter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.firsov.navalshooter.NavalShooter;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NavalShooter(), config);
+	}
+}
