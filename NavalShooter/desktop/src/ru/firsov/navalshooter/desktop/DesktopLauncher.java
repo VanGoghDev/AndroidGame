a56e4dc5555cec75d611fd3fb90203ac1af1aaff
VanGoghDev <kifirigor@gmail.com>
2018-09-18 23:50:47 +0300

    lesson2

--- NavalShooter/desktop/src/ru/firsov/navalshooter/desktop/DesktopLauncher.java
+++ NavalShooter/desktop/src/ru/firsov/navalshooter/desktop/DesktopLauncher.java
@@ -3,10 +3,11 @@ package ru.firsov.navalshooter.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import ru.firsov.navalshooter.NavalShooter;
+import ru.firsov.navalshooter.NavalShooter2DGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new NavalShooter(), config);
+		new LwjglApplication(new NavalShooter2DGame(), config);
 	}
 }
