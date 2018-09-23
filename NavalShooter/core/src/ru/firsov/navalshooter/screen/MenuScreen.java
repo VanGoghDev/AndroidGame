e191e499ea3cf3c7c162ba9288a3a37d93b7564a
VanGoghDev <kifirigor@gmail.com>
2018-09-23 22:17:03 +0300

    Set background

--- NavalShooter/core/src/ru/firsov/navalshooter/screen/MenuScreen.java
+++ NavalShooter/core/src/ru/firsov/navalshooter/screen/MenuScreen.java
@@ -4,21 +4,19 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
-import javax.swing.SpinnerDateModel;
-
 import ru.firsov.navalshooter.base.Base2DScreen;
+import ru.firsov.navalshooter.base.Sprite;
+import ru.firsov.navalshooter.math.Rect;
 
 public class MenuScreen extends Base2DScreen{
 
-    SpriteBatch batch;
-    Texture img;
+
+    Background background;
+    Texture bg;
     Vector2 pos;
-    Vector2 touch = new Vector2(0, 0);
-    Vector2 buf = new Vector2();
-    Vector2 way = new Vector2();
 
     public MenuScreen(Game game) {
         super(game);
@@ -27,11 +25,9 @@ public class MenuScreen extends Base2DScreen{
     @Override
     public void show(){
         super.show();
-        batch = new SpriteBatch();
-        img = new Texture("badlogic.jpg");
+        bg = new Texture("bg.jpg");
         pos = new Vector2(0f,0f);
-        buf = new Vector2(0f, 0f);
-
+        background = new Background(new TextureRegion(bg));
     }
 
     @Override
@@ -39,30 +35,20 @@ public class MenuScreen extends Base2DScreen{
         super.render(delta);
         Gdx.gl.glClearColor(0.3f, 0.4f, 0.7f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        buf.set(touch);
-        way = buf.sub(pos);
-        if (way.len() > 3f) {
-            pos.add(way.nor().scl(3));
-        } else {
-            pos.set(touch);
-        }
         batch.begin();
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
         batch.end();
     }
 
     @Override
-    public void dispose() {
-        batch.dispose();
-        img.dispose();
-        super.dispose();
+    protected void resize(Rect worldBounds) {
+        background.resize(worldBounds);
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        System.out.println("touchDown screenX = " + screenX + " screenY = "+ (Gdx.graphics.getHeight() - screenY));
-        touch.set(screenX, (Gdx.graphics.getHeight() - screenY));
-        return super.touchDown(screenX, screenY, pointer, button);
+    public void dispose() {
+        bg.dispose();
+        super.dispose();
     }
 
 }
