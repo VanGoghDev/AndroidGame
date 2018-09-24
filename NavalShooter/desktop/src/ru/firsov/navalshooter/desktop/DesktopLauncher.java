277d88697035ed5f5270e95cfbbc0b47d5ec5d70
VanGoghDev <kifirigor@gmail.com>
2018-09-24 17:22:19 +0300

    Quit Button added

--- NavalShooter/desktop/src/ru/firsov/navalshooter/desktop/DesktopLauncher.java
+++ NavalShooter/desktop/src/ru/firsov/navalshooter/desktop/DesktopLauncher.java
@@ -8,6 +8,10 @@ import ru.firsov.navalshooter.NavalShooter2DGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		float aspect = 3f / 4f;
+		config.resizable = false;
+		config.width = 500;
+		config.height = (int) (config.width / aspect);
 		new LwjglApplication(new NavalShooter2DGame(), config);
 	}
 }
